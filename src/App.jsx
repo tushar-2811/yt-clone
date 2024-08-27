063cc86ffe2c5c4a6dd0c296042a6f5cea46d333
tushar-2811 <tusharrawat52@gmail.com>
2024-08-27 16:22:30 +0530

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import  {BrowserRouter ,Route, Routes } from 'react-router-dom'
+import './App.css'
+import LandingPage from './pages/LandingPage'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+       <Routes>
+          <Route path='/' element={<LandingPage/>} />
+       </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+       <h1 className='text-red-500' >Welcome to Youtube</h1>
+    </div>
+  )
+}
+
+export default LandingPage
